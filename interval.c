ef713df7e2eef6e73eb1c46780881cd1750be35b
priyam1997 <30360706+priyam1997@users.noreply.github.com>
2018-04-08 18:54:58 +0530

    my interval with odd number prog

---
+++ interval.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,i;
+    printf("\n Enter 2 numbers :");
+    scanf("%d%d",&x,&y);
+    printf("\nOdd numbers are\n");
+    for(i=x;i<=y;i++)
+    {
+        if(i%2!=0)
+        {
+
+            printf("%d\t\n",i);
+        }
+    }
+    return 0;
+}
